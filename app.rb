249e9c54118cf2764d7d3fd43050eb4d87766fc7
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-02-01 18:44:13 +0200

    added sessions and helpers

--- app.rb
+++ app.rb
@@ -3,7 +3,19 @@ require 'sinatra'
 require 'slim'
 
 class Ranking < Sinatra::Application
+  enable :sessions
 
+  helpers do
+
+    def login?
+      return session[:username].nil?
+    end
+
+    def username
+      return session[:username]
+    end
+
+  end
 
 end
 
