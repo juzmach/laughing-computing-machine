cc4cb55a1fa3aae18964d207dda61d8a3d98f830
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-02-01 20:39:08 +0200

    added create and find methods

--- models/player.rb
+++ models/player.rb
@@ -4,4 +4,13 @@ class Player < Sequel::Model
   def self.all
     with_sql('SELECT * FROM players').all
   end
+
+  def self.create (username,password, name)
+    insert_dataset = DB["INSERT INTO players (username,password,name) VALUES (?,?,?)",username,password,name]
+    insert_dataset.insert
+  end
+
+  def self.find (id)
+    with_sql('SELECT * FROM players WHERE id = ?',id).first
+  end
 end
