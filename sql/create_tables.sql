073284bdd64a61284c50c381e850cc12fcc26c6f
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-02-08 23:45:38 +0200

    removed grants

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -35,12 +35,3 @@ CREATE TABLE teams (
     id                      serial      PRIMARY KEY,
     match_id                integer     REFERENCES matches(id) ON DELETE CASCADE
 );
-
-GRANT ALL PRIVILEGES ON TABLE players TO beerpong;
-GRANT ALL PRIVILEGES ON TABLE players_id_seq TO beerpong;
-GRANT ALL PRIVILEGES ON TABLE tournaments TO beerpong;
-GRANT ALL PRIVILEGES ON TABLE tournaments_id_seq TO beerpong;
-GRANT ALL PRIVILEGES ON TABLE matches TO beerpong;
-GRANT ALL PRIVILEGES ON TABLE matches_id_seq TO beerpong;
-GRANT ALL PRIVILEGES ON TABLE teams TO beerpong;
-GRANT ALL PRIVILEGES ON TABLE teams_id_seq TO beerpong;
