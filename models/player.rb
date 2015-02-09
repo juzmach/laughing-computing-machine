08c4a96d0b033de909d2183635bc56e16e451b46
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-02-09 22:57:49 +0200

    refactored player model to use DB.fetch and datasets for queries.

--- models/player.rb
+++ models/player.rb
@@ -2,31 +2,33 @@
 
 class Player < Sequel::Model
   def self.all
-    with_sql('SELECT * FROM players').all
-  end
-
-  def self.create (username,password, name)
-    insert_dataset = DB["INSERT INTO players (username,password,name) VALUES (?,?,?)",username,password,name]
-    insert_dataset.insert
-  end
-
-  def self.find_by_id (id)
-    with_sql('SELECT * FROM players WHERE id = ?',id).first
+    DB.fetch('SELECT * FROM players').all
   end
 
   def self.top10
-    with_sql('SELECT * FROM players ORDER BY ranking_score DESC LIMIT 10').all
+    DB.fetch('SELECT * FROM players ORDER BY ranking_score DESC LIMIT 10').all
+  end
+
+  def self.find_by_id (id)
+    DB.fetch('SELECT * FROM players WHERE id = ?',id).first
   end
 
   def self.find_by_username (username)
-    with_sql('SELECT * FROM players WHERE username LIKE ? LIMIT 1',username).first
+    DB.fetch('SELECT * FROM players WHERE username LIKE ? LIMIT 1',username).first
   end
 
-  def self.update(id)
+  def self.create (username,password, name)
+    insert_ds = DB['INSERT INTO players (username,password,name) VALUES (?,?,?)',username,password,name]
+    insert_ds.insert
+  end
 
+  def self.update(id, new_password,name)
+    update_ds = DB['UPDATE players SET password = ?, name = ? WHERE id = ?',new_password,name,id]
+    update_ds.update
   end
 
   def self.destroy(id)
-
+    delete_ds = DB['DELETE FROM players WHERE ID = ?',id]
+    delete_ds.delete
   end
 end
