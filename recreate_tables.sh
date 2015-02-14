c432e8678a43b97b5807acf70e3184d0fd70f703
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-02-15 00:07:57 +0200

    added grants back

--- recreate_tables.sh
+++ recreate_tables.sh
@@ -6,4 +6,8 @@ echo "Done."
 
 echo "Creating tables"
 psql -d beerpong < sql/create_tables.sql
+
+echo "Granting access"
+psql -d beerpong < sql/grant_access.sql
 echo "Done"
+
---
+++ sql/grant_access.sql
@@ -0,0 +1,8 @@
+GRANT ALL PRIVILEGES ON TABLE players TO beerpong;
+GRANT ALL PRIVILEGES ON TABLE players_id_seq TO beerpong;
+GRANT ALL PRIVILEGES ON TABLE tournaments TO beerpong;
+GRANT ALL PRIVILEGES ON TABLE tournaments_id_seq TO beerpong;
+GRANT ALL PRIVILEGES ON TABLE matches TO beerpong;
+GRANT ALL PRIVILEGES ON TABLE matches_id_seq TO beerpong;
+GRANT ALL PRIVILEGES ON TABLE teams TO beerpong;
+GRANT ALL PRIVILEGES ON TABLE teams_id_seq TO beerpong;
