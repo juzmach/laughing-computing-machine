c4e3f403329d791fa2d9ef7210bb65a6ee4baf08
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-02-15 02:12:33 +0200

    added team methods

--- models/match.rb
+++ models/match.rb
@@ -4,13 +4,17 @@ class Match < Sequel::Model
     DB.fetch('SELECT * FROM matches').all
   end
 
+  def self.find_by_id(id)
+    DB.fetch('SELECT * FROM matches WHERE id = ?',id).first
+  end
+
   def self.create (tournament_id,score_multiplier,team_a_id,team_b_id,match_date)
     is_tournament_match = false
     unless tournament_id.nil?
       is_tournament_match = true
     end
-    sql_query = 'INSERT INTO matches (is_tournament_match,tournament_id,score_multiplier,team_a_id,team_b_id,,match_date)
-                  VALUES (?,?,?,?,?,?,?,?)'
+    sql_query = 'INSERT INTO matches (is_tournament_match,tournament_id,score_multiplier,team_a_id,team_b_id,match_date)
+                  VALUES (?,?,?,?,?,?)'
     insert_ds = DB[sql_query,is_tournament_match,tournament_id,score_multiplier,team_a_id,team_b_id,match_date]
     insert_ds.insert
   end
@@ -27,7 +31,4 @@ class Match < Sequel::Model
     delete_ds.delete
   end
 
-  def self.find(id)
-    DB.fetch('SELECT * FROM matches WHERE id = ?',id).first
-  end
 end
--- models/team.rb
+++ models/team.rb
@@ -1,23 +1,29 @@
 class Team < Sequel::Model
 
   def self.all
-
+    DB.fetch('SELECT * FROM teams').all
   end
 
-  def self.create
-
+  def self.find_by_id(id)
+    DB.fetch('SELECT * FROM teams WHERE id = ?',id)
   end
 
-  def self.update(id)
+  def self.create(name,player_one_id,player_two_id)
+    sql_query = 'INSERT INTO teams (name,player_one_id,player_two_id) VALUES (?,?,?)'
+    insert_ds = DB[sql_query,player_one_id,player_two_id]
+    insert_ds.insert
+  end
 
+  def self.update(id,name,player_one_id,player_two_id)
+    sql_query = 'UPDATE teams SET name = ?, player_one_id = ?, player_two_id = ? WHERE id = ?'
+    update_ds = DB[sql_query,name,player_one_id,player_two_id,id]
+    update_ds.update
   end
 
   def self.destroy(id)
-
-  end
-  
-  def self.find(id)
-
+    sql_query = 'DELETE FROM teams WHERE id = ?'
+    delete_ds = DB[sql_query,id]
+    delete_ds.delete
   end
 
 end
