7f4f26ab32add47967fe31854ec8e0f14d820fa4
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-02-17 23:10:17 +0200

    changed name columns. Modified test data.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,25 +1,36 @@
-INSERT INTO players (username, password, name,is_admin,team_id) VALUES
-    ('ProduciveExecutioner','test123','Peter Hornberger',false,1),
-    ('Lemonator','test123','Elizabeth Lemon',true,2),
-    ('SilverPanther','princeton4ever','Jack Donaghy',true,3),
-    ('IGOTEGOTWHATUGOT','angie123','Tracy Jordan',false,4);
+INSERT INTO players (username, password, player_name,is_admin) VALUES
+    ('ProduciveExecutioner','test123','Peter Hornberger',false),
+    ('Lemonator','test123','Elizabeth Lemon',true),
+    ('SilverPanther','princeton4ever','Jack Donaghy',true),
+    ('IGOTEGOTWHATUGOT','angie123','Tracy Jordan',false),
+    ('Heisenberg','SayMyName','Walter White',false),
+    ('SUCKITBITCH!','CapnCook','Jesse Pinkman',false),
+    ('CriminalLawyer','BetterCallSaul','Saul Goodman',false),
+    ('Mike','Kaylee123','Mike Ehrmantraut',false),
+    ('HermanoPollo','h3rm4n0s','Gustavo Fring',false),
+    ('King2Co','Respect123','Tuco Salamanca',false);
 
-INSERT INTO tournaments (name,start_date,end_date,start_time,end_time,location,admin_id) VALUES
-    ('30 Rock Annual Championships','2015-01-25','2015-01-25','12:00:00','23:00:00','30 Rockefeller Plaza, New York, NY 10112',3),
-    ('The REAL Championships that dont suck!','2015-01-26','2015-01-26','00:00:00','06:00:00','The Writers Room',2);
+INSERT INTO teams (team_name,player_one_id,player_two_id) VALUES
+    ('The Playahs',3,4),
+    ('The Nerds',1,2),
+    ('The Cooks',5,6),
+    ('The Problem Solvers',7,8),
+    ('HermanoPollo',9,null),
+    ('King2Co',10,null);
 
-INSERT INTO matches (is_tournament_match,tournament_id,score_multiplier) VALUES
-    (true,1,1.5),
-    (true,1,1.5),
-    (true,1,1.5),
-    (true,1,1.5);
+INSERT INTO tournaments (tournament_name,start_date,start_time,end_date,end_time,location,admin_id) VALUES
+    ('30 Rock Annual Championships','2015-03-25','12:00:00','2015-03-25','23:00:00','30 Rockefeller Plaza, New York, NY 10112',3),
+    ('The REAL Championships that dont suck!','2015-03-26','00:00:00','2015-03-26','06:00:00','The Writers Room',2);
 
-INSERT INTO matches (is_tournament_match) VALUES
-    (false);
+INSERT INTO matches (is_tournament_match,tournament_id,score_multiplier,team_a_id,team_b_id,match_date,status) VALUES
+    (true,1,1.5,1,2,'2015-03-25','Confirmed'),
+    (true,1,1.5,3,4,'2015-03-25','Confirmed'),
+    (true,1,1.5,1,4,'2015-03-25','Confirmed'),
+    (true,1,1.5,2,3,'2015-03-25','Confirmed'),
+    (false,1,1.0,5,6,'2015-02-02','Confirmed');
 
-INSERT INTO teams (match_id) VALUES
-    (1),
-    (2),
-    (3),
-    (4),
-    (5);
+UPDATE matches SET
+    team_a_score = 8,
+    team_b_score = 0,
+    status = 'Completed'
+    WHERE id = 5;
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,38 +2,37 @@ CREATE TABLE players (
     id                      serial        PRIMARY KEY,
     username                varchar(200)  NOT NULL UNIQUE,
     password                char(60)      NOT NULL,
-    name                    varchar(200)  NOT NULL,
+    player_name             varchar(200)  NOT NULL,
     ranking_score           numeric       NOT NULL DEFAULT 1000.0,
     games_played            integer       NOT NULL DEFAULT 0,
     wins                    integer       NOT NULL DEFAULT 0,
     losses                  integer       NOT NULL DEFAULT 0,
     ties                    integer       NOT NULL DEFAULT 0,
     is_admin                boolean       NOT NULL DEFAULT false,
-    team_id                 integer,
-    created_at              timestamp,
-    updated_at              timestamp
+    created_at              timestamp     DEFAULT current_timestamp,
+    updated_at              timestamp     DEFAULT current_timestamp
 );
 
 CREATE TABLE teams (
     id                      serial        PRIMARY KEY,
-    name                    varchar(200)  NOT NULL,
+    team_name               varchar(200)  NOT NULL,
     player_one_id           integer       NOT NULL,
     player_two_id           integer,
-    created_at              timestamp,
-    updated_at              timestamp
+    created_at              timestamp     DEFAULT current_timestamp,
+    updated_at              timestamp     DEFAULT current_timestamp
 );
 
 CREATE TABLE tournaments (
     id                      serial        PRIMARY KEY,
-    name                    varchar(50)   NOT NULL UNIQUE,
+    tournament_name         varchar(50)   NOT NULL UNIQUE,
     start_date              date          NOT NULL,
     start_time              time          NOT NULL,
     end_date                date          NOT NULL,
     end_time                time          NOT NULL,
     location                varchar(50),
     admin_id                integer       NOT NULL REFERENCES players(id) ON DELETE CASCADE,
-    created_at              timestamp,
-    updated_at              timestamp
+    created_at              timestamp     DEFAULT current_timestamp,
+    updated_at              timestamp     DEFAULT current_timestamp
 );
 
 CREATE TABLE matches (
@@ -45,8 +44,8 @@ CREATE TABLE matches (
     team_b_id               integer       REFERENCES teams(id) ON DELETE CASCADE,
     team_a_score            integer       DEFAULT 0,
     team_b_score            integer       DEFAULT 0,
-    status                  integer       DEFAULT 0,
+    status                  varchar(50)   DEFAULT 'Challenged',
     match_date              date,
-    created_at              timestamp,
-    updated_at              timestamp
+    created_at              timestamp     DEFAULT current_timestamp,
+    updated_at              timestamp     DEFAULT current_timestamp
 );
